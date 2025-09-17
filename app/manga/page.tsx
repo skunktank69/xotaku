404ae38e5794c0fa9ad605ddadc7652d12d42d55
apurvsikka <apurvsikka@gmail.com>
2025-09-17 22:56:04 +0530

    🎐 fixed manga home page top padding

--- app/manga/page.tsx
+++ app/manga/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
   }, []);
   return (
     <>
-      <div className="lg:p-6 lg:pt-20 ">
+      <div className="lg:p-6 lg:pt-8">
         <MangaSlider />
         {/* <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div> */}
         <PopularManga latestManga={popularManga} />
